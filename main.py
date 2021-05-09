af03299407c14f1682d8cd21a0cccb6b5e71eac3
OliverDremak <72332685+OliverDremak@users.noreply.github.com>
2021-05-09 10:50:49 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+alap=int(input("Kérem a háromszög alapját:"))
+magasság=int(input("Kérem a háromszög magasságát:"))
+a=int(input("Kérem a háromszög b oldalát:"))
+b=int(input("Kérem a háromszög a oldalát:"))
+c=int(input("Kérem a háromszög c oldalát:"))
+
+terület=alap*magasság/2
+kerület=a+b+c
+
+print(f'Területe:{terület}')
+print(f'Kerülete:{kerület}')
